42f1928a0d41956f0415befe9ede450c57ee61d6
潇洒 <liuxiaosa@higohappy.com>
2019-04-25 13:38:41 +0800

    更新数据库地址

--- server/config/sequelizeBase.js
+++ server/config/sequelizeBase.js
@@ -1,7 +1,7 @@
 //sequelize基础配置文件
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('mall','root','xiaosa98',{
-	host:'127.0.0.1',
+	host:'192.144.186.149',
 	dialect:'mysql',
 })
 
