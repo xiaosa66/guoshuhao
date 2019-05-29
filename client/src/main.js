44cd8329a8f2900d362728c9228c7115406e1ccc
潇洒 <liuxiaosa@higohappy.com>
2019-05-29 23:46:06 +0800

    jaja

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,7 @@ import 'babel-polyfill';
 import Vue from 'vue';
 import App from './App';
 import router from './router/client';
-import iView from 'iview';
-import 'iview/dist/styles/iview.css';
-// import router from './router/admin';
+
 import store from './store';
 
 
@@ -12,7 +10,6 @@ import './assets/css/common.less';
 import './assets/font/iconfont.css';
 
 Vue.config.productionTip = false;
-Vue.use(iView);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
